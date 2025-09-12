52ff159f21cb0628475a4bd1307bc6a5f36cd3f0
scare-destiny <zhenya.venger@gmail.com>
2025-09-12 17:50:44 +0200

    chore: remove pnpm-lock.yaml and update site configuration
    - Deleted pnpm-lock.yaml to clean up unused lock file.
    - Updated site configuration: changed website URL to "https://recipes.www.venger.me/" and timezone to "Europe/Berlin".

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,9 @@
 export const SITE = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
-  author: "Sat Naing",
-  profile: "https://satnaing.dev/",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
+  website: "https://recipes.www.venger.me/", // replace this with your deployed domain
+  author: "Eugene Venger",
+  profile: "https://www.venger.me/",
+  desc: "Recipes for the everyday home cook - healthy and sooo tasty.",
+  title: "Recipes",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerIndex: 4,
@@ -19,5 +19,5 @@ export const SITE = {
   dynamicOgImage: true,
   dir: "ltr", // "rtl" | "auto"
   lang: "en", // html lang code. Set this empty and default will be "en"
-  timezone: "Asia/Bangkok", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+  timezone: "Europe/Berlin", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
 } as const;
